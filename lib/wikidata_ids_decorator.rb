d7a8aef232592813fac413189145e1f5af0389ed
Chris Mytton <chrismytton@gmail.com>
2017-03-13 14:15:29 +0100

    Add initial version of decorator from prototype
    This is from the Saint Lucia scraper, where this idea was first
    prototyped.

--- lib/wikidata_ids_decorator.rb
+++ lib/wikidata_ids_decorator.rb
@@ -1,5 +1,23 @@
 require 'wikidata_ids_decorator/version'
+require 'scraped'
+require 'wikidata/fetcher'
 
-module WikidataIdsDecorator
-  # Your code goes here...
+class WikidataIdsDecorator < Scraped::Response::Decorator
+  def body
+    Nokogiri::HTML(super).tap do |doc|
+      links = doc.css('#bodyContent a[href*="/wiki"][title]').reject { |a| a[:title].include? ':' }
+      wdids = wikidata_ids(links.map { |a| a[:title] }.uniq)
+      links.each { |a| a[:wikidata] = wdids[a[:title]] }
+    end.to_s
+  end
+
+  private
+
+  def wikidata_ids(links)
+    WikiData.ids_from_pages(language, links)
+  end
+
+  def language
+    'en'
+  end
 end
--- test/wikidata_ids_decorator_test.rb
+++ test/wikidata_ids_decorator_test.rb
@@ -4,4 +4,25 @@ describe WikidataIdsDecorator do
   it 'has a version number' do
     ::WikidataIdsDecorator::VERSION.wont_be_nil
   end
+
+  describe 'Wikipedia page with links' do
+    it 'adds a data-wikidata attribute to links' do
+      body = <<-HTML
+<div id="bodyContent">
+  <a href="https://en.wikipedia.org/wiki/Douglas_Adams" title="Douglas Adams">Douglas Adams</a>
+</div>
+      HTML
+      response = Scraped::Response.new(body: body, url: 'https://en.wikipedia.org/wiki/Douglas_Adams_(disambiguation)')
+      decorator = WikidataIdsDecorator.new(response: response)
+      decorator.body.must_include '<a href="https://en.wikipedia.org/wiki/Douglas_Adams" title="Douglas Adams"' \
+        ' wikidata="Q42">Douglas Adams</a>'
+    end
+  end
+
+  describe 'URLs containing a colon' do
+    it 'it should leave links not pointing to an article alone' do
+      decorator.body.must_include '<a href="/wiki/Special:BookSources/0-330-26738-8" '\
+                                  'title="Special:BookSources/0-330-26738-8">0-330-26738-8</a>'
+    end
+  end
 end
