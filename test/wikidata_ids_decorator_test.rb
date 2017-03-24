006aff31fbf08ea8f0c6b7ac54114385e1a29d89
Oliver Denman <oliver@mysociety.org>
2017-03-24 09:57:52 +0000

    fixup! Add initial version of decorator from prototype

--- test/wikidata_ids_decorator_test.rb
+++ test/wikidata_ids_decorator_test.rb
@@ -12,18 +12,15 @@ describe WikidataIdsDecorator::Links do
   around { |test| VCR.use_cassette(File.basename(url), &test) }
   let(:response) { Scraped::Request.new(url: url).response }
   let(:decorator) { WikidataIdsDecorator::Links.new(response: response) }
+  let(:url) { 'https://en.wikipedia.org/wiki/Life,_the_Universe_and_Everything' }
 
   describe 'Wikipedia page with links' do
-    let(:url) { 'https://en.wikipedia.org/wiki/Douglas_Adams_(disambiguation)' }
-
     it 'adds a data-wikidata attribute to links' do
       decorator.body.must_include '<a href="/wiki/Douglas_Adams" title="Douglas Adams" wikidata="Q42">'
     end
   end
 
   describe 'Anchored links' do
-    let(:url) { 'https://en.wikipedia.org/wiki/Life,_the_Universe_and_Everything' }
-
     it 'adds a data-wikidata attribute to anchored links' do
       decorator.body.must_include '<a href="/wiki/OCLC#Identifiers_and_linked_data" title="OCLC" wikidata="Q190593">'
     end
@@ -34,16 +31,12 @@ describe WikidataIdsDecorator::Links do
   end
 
   describe 'Redirect links' do
-    let(:url) { 'https://en.wikipedia.org/wiki/Life,_the_Universe_and_Everything' }
-
-    it 'adds a data-wikidata attribute to redirect <links>  </links>' do
+    it 'it follows the redirect and adds the correct wikidata item id' do
       decorator.body.must_include '<a href="/wiki/Vogons" class="mw-redirect" title="Vogons" wikidata="Q1076157">'
     end
   end
 
   describe 'Links outside of #bodyContent' do
-    let(:url) { 'https://en.wikipedia.org/wiki/Life,_the_Universe_and_Everything' }
-
     it 'should leave these links alone' do
       decorator.body.must_include '<a href="/wiki/Wikipedia:About" title="Wikipedia:About">About Wikipedia</a>'
     end
